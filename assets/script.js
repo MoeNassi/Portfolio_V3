ad3d6348c5b032fc0366cf4d90fdb4ffa94eccbb
medaitswa <medaitswa@gmail.com>
2024-03-09 18:00:21 +0100

    done

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
 const tl = gsap.timeline({default : {ease : 'power2.out'}});
 
 tl.fromTo(".slider", {x: "0%"}, {x: "-200%"}, duration = .5)
+
+function copy() {
+	var text = "sngp567@gmail.com"
+	navigator.clipboard.writeText(text)
+	.then(()=> {
+		element.textContent = "copied"
+		element.classList.add('copy')
+	})
+	.catch(()=> {
+		element.textContent = "failed"
+		element.classList.add('err')
+	})
+	var element = document.createElement('div')
+	document.body.appendChild(element)
+	element.classList.add('show')
+	setTimeout(()=> element.classList.remove('show'), 1000)
+}
